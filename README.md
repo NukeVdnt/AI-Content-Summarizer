58612fcd20d4b595dc031f700408ba4ffbfe8152
NukeVdnt <vedvnukte@gmail.com>
2024-01-10 21:40:32 +0530

    adding files to repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# TLDRIFY
+
+AI content summarizer, summarizes articles as well as website in a single paragraph.
+
+### Tech Stack
+
+Front-End - ReactJs\
+Api - RapidApi, ReduxJs-Toolkit
+
+### Snap
---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { logo } from "../assets/";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="tldrify logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open("#");
+          }}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        <span className="blue_gradient">TL;DR? </span>
+        <br className="max-md: hidden" />
+        We got you covered.
+      </h1>
+      <h2 className="desc">
+        Don't Fear the Deep Dive: We'll Hold Your Hand Through the TL;DR Sea.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
